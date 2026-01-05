30d93ebe10bc2b8f2c35570e9535a3c6fd246b80
Itttikorn <ittikornsd@gmail.com>
2026-01-05 13:36:36 +0700

    feat: add ProfileCard component and pass data via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import ProfileCard from './components/ProfileCard.jsx'
+
 function App() {
 
   return (
-    <div>
-      <h1>My First React App</h1>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px', marginTop: '50px' }}>
+      <h1>My Team Portfolio</h1>
+
+      <ProfileCard
+        name="อิทธิกร พรชัยพิมบพันธ์"
+        role="Student @ CEDT"
+        bio="a"
+      />
+
+      <ProfileCard
+        name="John Doe"
+        role="Guest Developer"
+        bio="I love coding and learning new things."
+      />
     </div>
-  )
+  );
 }
 
 export default App
